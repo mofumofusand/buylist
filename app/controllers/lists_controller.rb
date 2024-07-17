146de1dec58ba62337d201c4de284ef285d3ae63
mofumofusand <sy12031008@gmail.com>
2024-07-18 01:39:54 +0900

    formオブジェクト

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,6 @@
 class ListsController < ApplicationController
   #before_action :authenticate_user!
+  before_action :set_list, only: [:edit, :update]
   before_action :move_to_index, except: [:index, :show]
 
 
@@ -21,10 +22,41 @@ class ListsController < ApplicationController
     end
   end
 
+  def edit
+    list_attributes = @list.attributes
+    @list_form = ListForm.new(list_attributes)
+  end
+
+  def update
+    @list_form = ListForm.new(list_form_params)
+    if @list_form.valid?
+      @list_form.update(list_form_params, @list)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+
+  def destroy
+    list = List.find(params[:id])
+    if current_user.id == list.user_id
+      list.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+
   private
 
   def list_form_params
     params.require(:list_form).permit(:content, :quantity).merge(user_id: current_user.id)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
   end
 
   def move_to_index
--- app/models/list_form.rb
+++ app/models/list_form.rb
@@ -1,12 +1,16 @@
 class ListForm
   include ActiveModel::Model
-  
-  attr_accessor :content, :quantity, :user_id
+
+  attr_accessor :content, :quantity, :user_id, :id, :created_at, :updated_at
 
   validates :content, presence: true
   validates :quantity, presence: true
 
   def save
     List.create(content: content, quantity: quantity, user_id: user_id)
+  end
+
+  def update(params, list)
+    list.update(params)
   end
 end
