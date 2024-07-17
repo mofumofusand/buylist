2f4070e3cc978c17e54918390cdbb7b381543d40
mofumofusand <sy12031008@gmail.com>
2024-07-17 14:36:21 +0900

    ユーザー登録

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,17 @@
+class ListsController < ApplicationController
+  #before_action :authenticate_user!
+  before_action :move_to_index, except: [:index, :show]
+
+
+  def index
+  end
+
+
+  private
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+end
---
+++ db/migrate/20240716120038_create_lists.rb
@@ -0,0 +1,10 @@
+class CreateLists < ActiveRecord::Migration[7.0]
+  def change
+    create_table :lists do |t|
+      t.string :content, null: false
+      t.string :quantity
+      t.references :user,          null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
