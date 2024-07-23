d9e7260de18441663b327ff6b5521450aa28c47d
mofumofusand <sy12031008@gmail.com>
2024-07-23 16:57:37 +0900

    修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,4 +5,8 @@ class Category < ApplicationRecord
   has_many :lists, through: :category_lists
 
   validates :tag_name, uniqueness: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["tag_name"]
+  end
 end
