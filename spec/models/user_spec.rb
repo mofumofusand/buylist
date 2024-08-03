70160fd130ed780a652fb890ea551786e5b478d0
mofumofusand <sy12031008@gmail.com>
2024-08-03 22:42:34 +0900

    README URL追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'ユーザー新規登録' do
+  before do
+    @user = FactoryBot.build(:user)
+  end
 
+  describe 'ユーザー新規登録' do
+    context '新規登録できるとき' do
+      it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+    context '新規登録できないとき' do
+      it 'nicknameが空では登録できない' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it 'emailが空では登録できない' do
+
+      end
+      it '重複したemailが存在する場合は登録できない' do
+        
+      end
+      it 'mailは@を含まないと登録できない' do
+        
+      end
+      it 'passwordが空では登録できない' do
+        
+      end
+      it 'passwordが5文字以下では登録できない' do
+        
+      end
+      it 'passwordが全角では登録できない' do
+        
+      end
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+        
+      end
+    end
   end
 end
